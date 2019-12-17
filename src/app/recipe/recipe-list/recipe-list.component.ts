ff4483df1b865f400fe421c225a125d52302e7af
OleksandrParkhomenko <oleksandr.parhomenko@yahoo.com>
2019-12-17 15:18:38 +0200

    recipe component added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Strudel', 'Using this recipe you can cook a delicious strudel in less than two hours',
+    'https://elavegan.com/wp-content/uploads/2019/10/easy-apple-strudel-with-raisins-and-walnuts.jpg'),
+    new Recipe('Cheesecake', 'New York Cheesecake Recipe for Begginers',
+    'https://cookiesandcups.com/wp-content/uploads/2017/10/perfectcheesecake-13-1.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
